7739cdf5f5f6770d12e50ed5183762ff215aadb7
armandomm09 <pabloarmando.macbeath@gmail.com>
2024-01-03 21:25:54 -0600

    Initial Commit

---
+++ Vortex App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vortex App
+//
+//  Created by Pablo Armando Mac Beath Milián on 03/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vortex App/Vortex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vortex_AppApp.swift
+//  Vortex App
+//
+//  Created by Pablo Armando Mac Beath Milián on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Vortex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
