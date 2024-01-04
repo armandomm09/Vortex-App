9a1b11391a66226a5ff4e72910dbc094f5d0cb19
armandomm09 <pabloarmando.macbeath@gmail.com>
2024-01-03 23:14:00 -0600

    First

--- Vortex App/ContentView.swift
+++ Vortex App/ContentView.swift
@@ -1,24 +1,90 @@
-//
-//  ContentView.swift
-//  Vortex App
-//
-//  Created by Pablo Armando Mac Beath Milián on 03/01/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var teamName: String = ""
+    
+    var languagesPhotos: [String] = [
+        "javaLogo",
+        "cppLogo",
+        "labviewLogo",
+        "pythonLogo"
+    ]
+    
+    @State private var selectedOption = 0
+    
+    let chassisTypes: [String] = [
+        "Tank",
+        "Mecanum",
+        "Swerve",
+        "Dragonfly"
+    ]
+    
+    @State var chassisSelection = ""
+    
+    let wheelTypes: [String] = [
+    "Omni",
+    "Colsom",
+    "KOP",
+    "Plaction",
+    "Pneumatic"
+    ]
+    
+    @State var wheelSelection = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        
+           
+        NavigationView {
+            Form {
+                TextField("Team Name", text: $teamName)
+                    .padding()
+                    .foregroundColor(.white)
+                    .bold()
+                    //.background(Color.red)
+                    //.cornerRadius(10)
+                
+                HStack{
+                    ForEach(languagesPhotos, id: \.self) {imageName in
+                        Button {
+                        print("Boton \(imageName)")
+                        } label: {
+                            Image(imageName)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50)
+                        }
+                        .padding()
 
-#Preview {
-    ContentView()
+                    }
+
+                }
+                
+                
+                Section {
+                    Picker("Chassis", selection: $chassisSelection) {
+                        ForEach(chassisTypes, id: \.self){chassisType in
+                            Text(chassisType)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    
+                    Picker("Wheel", selection: $wheelSelection) {
+                        ForEach(wheelTypes, id: \.self){wheelType in
+                            Text(wheelType)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+              
+            }
+            .navigationTitle("Team: ")
+        
+  }
+}
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
